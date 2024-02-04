4eb33e6a928fe1b0cc112b5d35ef89a389e0323b
vatume <96790678+vatume@users.noreply.github.com>
2024-02-03 23:57:41 -0500

    ready for render2

--- projects/models.py
+++ projects/models.py
@@ -3,5 +3,4 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     weight = models.IntegerField()
-    classification = models.CharField(max_length=3)
     created_at = models.DateTimeField(auto_now_add=True)
